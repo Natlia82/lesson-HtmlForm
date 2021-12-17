e148da4a2c14f0671f841d950a1d61c6378ea5d0
Natalia <nata@host>
2021-12-17 16:37:10 +0500

    сообщение готово

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,32 @@
 const buttonClick = document.getElementsByClassName('buttonPopovers');
 //const contener = document.querySelector('.contenerButton');
 buttonClick[0].addEventListener('click', (e) => {
+    if (document.querySelector('.form-hint')) {
+        DeleteElemtnt();
+        return;
+    }
+    AddElement(e);
+
+});
+
+//добавлнеи элемента
+function AddElement(e) {
     let newElement = document.createElement('div');
     let newH2 = document.createElement('h4');
     newH2.classList.add('newH');
     newH2.textContent = 'ФСБ'; //.appendChild(document.createTextNode('ghghg'));
     newElement.classList.add('form-hint');
-    newElement.textContent = buttonClick[0].getAttribute('data-text');
+    newElement.textContent = e.target.getAttribute('data-text');
     const widthBut = e.target.clientWidth;
     newElement.style.width = `${widthBut}px`;
     newElement.insertAdjacentElement('afterbegin', newH2);
     e.target.insertAdjacentElement('afterbegin', newElement);
-    console.log(newElement);
-});
+    // console.log(newElement);
+}
+
+//удаленеи элемента
+function DeleteElemtnt() {
+    const elem = document.querySelector('.form-hint');
+    elem.remove();
+
+}
