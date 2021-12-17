5ad7d9e5b1dae8c308b1426c46db2b4c1995cd09
Natalia <nata@host>
2021-12-17 17:25:28 +0500

    lint

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -3,28 +3,28 @@ import puppetteer from 'puppeteer';
 jest.setTimeout(30000); // default puppeteer timeout
 
 describe('INN/OGRN form', () => {
-    let browser = null;
-    let page = null;
-    const baseUrl = 'http://localhost:9000';
+  let browser = null;
+  let page = null;
+  const baseUrl = 'http://localhost:9000';
 
-    beforeAll(async() => {
-        browser = await puppetteer.launch({
-            headless: false, // show gui
-            slowMo: 500,
-            devtools: true, // show devTools
-        });
-        page = await browser.newPage();
+  beforeAll(async () => {
+    browser = await puppetteer.launch({
+      headless: false, // show gui
+      slowMo: 500,
+      devtools: true, // show devTools
     });
+    page = await browser.newPage();
+  });
 
-    afterAll(async() => {
-        await browser.close();
-    });
+  afterAll(async () => {
+    await browser.close();
+  });
 
-    test('should add .valid class for valid inn', async() => {
-        await page.goto(baseUrl);
-        const form = await page.$('[data-widget=widget]');
-        const submit = await form.$('[data-submit=submit]');
-        submit.click();
-        await page.waitForSelector('.form-hint');
-    });
+  test('should add .valid class for valid inn', async () => {
+    await page.goto(baseUrl);
+    const form = await page.$('[data-widget=widget]');
+    const submit = await form.$('[data-submit=submit]');
+    submit.click();
+    await page.waitForSelector('.form-hint');
+  });
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,31 @@
 // TODO: write code here
 const buttonClick = document.getElementsByClassName('buttonPopovers');
-//const contener = document.querySelector('.contenerButton');
-buttonClick[0].addEventListener('click', (e) => {
-    if (document.querySelector('.form-hint')) {
-        DeleteElemtnt();
-        return;
-    }
-    AddElement(e);
-
-});
-
-//добавлнеи элемента
+// добавлнеи элемента
 function AddElement(e) {
-    let newElement = document.createElement('div');
-    let newH2 = document.createElement('h4');
-    newH2.classList.add('newH');
-    newH2.textContent = 'ФСБ'; //.appendChild(document.createTextNode('ghghg'));
-    newElement.classList.add('form-hint');
-    newElement.textContent = e.target.getAttribute('data-text');
-    const widthBut = e.target.clientWidth;
-    newElement.style.width = `${widthBut}px`;
-    newElement.insertAdjacentElement('afterbegin', newH2);
-    e.target.insertAdjacentElement('afterbegin', newElement);
-    // console.log(newElement);
+  const newElement = document.createElement('div');
+  const newH2 = document.createElement('h4');
+  newH2.classList.add('newH');
+  newH2.textContent = 'ФСБ'; // .appendChild(document.createTextNode('ghghg'));
+  newElement.classList.add('form-hint');
+  newElement.textContent = e.target.getAttribute('data-text');
+  const widthBut = e.target.clientWidth;
+  newElement.style.width = `${widthBut}px`;
+  newElement.insertAdjacentElement('afterbegin', newH2);
+  e.target.insertAdjacentElement('afterbegin', newElement);
+  // console.log(newElement);
 }
 
-//удаленеи элемента
+// удаленеи элемента
 function DeleteElemtnt() {
-    const elem = document.querySelector('.form-hint');
-    elem.remove();
-
+  const elem = document.querySelector('.form-hint');
+  elem.remove();
 }
+
+// const contener = document.querySelector('.contenerButton');
+buttonClick[0].addEventListener('click', (e) => {
+  if (document.querySelector('.form-hint')) {
+    DeleteElemtnt();
+    return;
+  }
+  AddElement(e);
+});
